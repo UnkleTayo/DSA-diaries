45e126776f4ad1edf7fdfd4dcc9ccdd5ef8e43b8
Adetayo <akinsanyaadetayo7@gmail.com>
2022-06-18 09:58:01 +0100

    Recurrsive  binary Search

--- binarySearch.js
+++ binarySearch.js
@@ -17,4 +17,26 @@ function binarySearch(arr, elm) {
 
   return -1;
 }
-console.log(binarySearch([2, 5, 6, 9, 10, 16, 35, 69, 585, 900], 900));
+
+function recursiveBinarySearch(arr, elm) {
+  return search(arr, elm, 0, arr.length - 1);
+}
+
+function search(arr, elm, start, end) {
+  if (start > end) {
+    return -1;
+  }
+
+  let middle = Math.floor((start + end) / 2);
+
+  if (elm === arr[middle]) {
+    return middle;
+  }
+  if (elm < arr[middle]) {
+    return search(arr, target, start, middle - 1);
+  } else if (elm > arr[middle]) {
+    return search(arr, elm, middle + 1, end);
+  }
+}
+
+console.log(recursiveBinarySearch([2, 5, 6, 9, 10, 16, 35, 69, 585, 900], 900));
