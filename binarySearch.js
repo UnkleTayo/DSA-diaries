d6fb3db0ba5ae6dd16d57030904aeea3b43d7f2b
Adetayo <akinsanyaadetayo7@gmail.com>
2022-06-18 09:43:36 +0100

    Itrerative binary Search

--- binarySearch.js
+++ binarySearch.js
@@ -1,17 +1,20 @@
 function binarySearch(arr, elm) {
   let start = 0;
   let end = arr.length - 1;
-  var middle = Math.floor((start + end) / 2);
 
-  while (arr[middle] !== elm) {
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+    if (arr[middle] === elm) {
+      return middle;
+    }
+
     if (elm < arr[middle]) {
       end = middle - 1;
     } else {
       start = middle + 1;
     }
-    var middle = Math.floor((start + end) / 2);
-    console.log(start, middle, end);
   }
-}
 
-binarySearch([2, 5, 6, 7, 9, 10, 16, 35, 69, 585, 900], 7);
+  return -1;
+}
+console.log(binarySearch([2, 5, 6, 9, 10, 16, 35, 69, 585, 900], 900));
